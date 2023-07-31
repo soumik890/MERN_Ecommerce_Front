ecbf0d3692a17cdc81bba2dd1d658c345d710817
Soumik <soumik890@gmail.com>
2023-07-31 18:06:33 +0530

    first commit

---
+++ src/pages/AdminProductDetailPage.jsx
@@ -0,0 +1,13 @@
+import AdminProductDetail from "../features/admin/AdminProductDetail";
+import NavBar from "../features/navbar/Navbar";
+function AdminProductDetailPage() {
+  return (
+    <div>
+      <NavBar>
+        <AdminProductDetail></AdminProductDetail>
+      </NavBar>
+    </div>
+  );
+}
+
+export default AdminProductDetailPage;
